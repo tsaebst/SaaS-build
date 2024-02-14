3c770b195ce609bd0852c57dd8c8f9a666705b92
tsaebst <125909246+tsaebst@users.noreply.github.com>
2024-02-14 22:41:27 +0200

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -25,15 +25,9 @@ API_TOKEN = "Medeya2017!"
 #your personal key here
 key = "RCWWMPLADXCCSTWC9Q6KCL9PG"
 
-
-# url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date_str}?key={key}"
-
-def generate_weather(exclude: str, limit: int = 1):
-    #трошки кастиль
-    json_data = request.get_json()
-    date = json_data.get("date")
-    date = format_date(date)
-    location = json_data.get("location")
+# request data from api using your personal key
+# use the link to create your own one: https://www.visualcrossing.com/account
+def generate_weather(location: str, date: str):
     url = f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date}?include=days&key={key}"
 
     headers = {"X-Api-Key": key}
@@ -44,6 +38,7 @@ def generate_weather(exclude: str, limit: int = 1):
         return json.loads(response.text)
     else:
         raise InvalidUsage(response.text, status_code=response.status_code)
+
 
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
@@ -56,20 +51,23 @@ def handle_invalid_usage(error):
 def home_page():
     return "<p><h2>KMA L2: python Saas.</h2></p>"
 
-
+# additional functions for changing data to the correct forms
 def format_date(timestamp):
     return timestamp.split("T")[0]
 
 def fahrenheit_to_celsius(t):
     return (t - 32) * 5.0/9.0
 
+def to_milibars(value):
+    return (value/10000)
 
+# function for dict creating
 def extract_weather_data(data, requester_name, location, date):
     weather_data = data['days'][0]
     weather = {
         "temp_c": fahrenheit_to_celsius(weather_data['temp']),
         "wind_kph": weather_data['windspeed'],
-        "pressure_mb": weather_data['pressure'],
+        "pressure_mb": to_milibars(weather_data['pressure']),
         "humidity": weather_data['humidity'],
     }
     result = {
@@ -81,18 +79,18 @@ def extract_weather_data(data, requester_name, location, date):
     }
     return result
 
-
-@app.route("/content/api/v1/integration/generateW", methods=["POST"])
+#main method for componising our data
+@app.route("/link", methods=["POST"])
 def weather_endpoint():
     json_data = request.get_json()
 
     if json_data is None:
         raise InvalidUsage("Request data is missing or not in JSON format", status_code=400)
-
+    # getting data from json doc and creating new vars using it
     date = json_data.get("date")
     requester_name = json_data.get("requester_name")
     location = json_data.get("location")
-
+    # checking if toker is correct and is present, checking if other required members are present, etc.
     if date is None or requester_name is None or location is None:
         raise InvalidUsage("date, requester_name, and location are required fields", status_code=400)
 
@@ -101,9 +99,9 @@ def weather_endpoint():
     if token != API_TOKEN:
         raise InvalidUsage("wrong API token", status_code=403)
 
-    exclude = json_data.get("exclude", "")
-
-    weather = generate_weather(exclude)
+    #collecting data as an response to our request
+    weather = generate_weather(location, date)
+    # creating right output format via our data
     data = extract_weather_data(weather, requester_name, location, date)
 
     return data
